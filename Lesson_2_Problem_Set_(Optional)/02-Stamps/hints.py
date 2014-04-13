a8428d024143b3c652b6935264bc25ff3abe5919
Lucy Bain <lucybain@gmail.com>
2014-04-13 17:18:23 +1000

    Create hint and solution files with directions for every exercise section.

---
+++ Lesson_2_Problem_Set_(Optional)/02-Stamps/hints.py
@@ -0,0 +1,6 @@
+# Please write HINTS here
+
+# From the README:
+# We do NOT accept pull requests that have deleted another contributer's hint or solution without a very clear reason
+# ONLY use concepts covered in the class so far
+# Do NOT give solutions here - ONLY hints
--- Lesson_4_Problem_Set/04-Better_Splitting/solutions.py
+++ Lesson_4_Problem_Set/04-Better_Splitting/solutions.py
@@ -1,3 +1,12 @@
+# Please write SOLUTIONS here
+
+# From the README:
+# We do NOT accept pull requests that have deleted another contributer's hint or solution without a very clear reason
+# ALL solutions must be clearly documented
+# ALL solutions must actually work
+# ONLY use concepts covered in the class so far
+
+
 # Solution by: Nadia Vu
 
 def split_string(source,splitlist):
