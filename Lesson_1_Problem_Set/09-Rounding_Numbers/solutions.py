6164e56f7eabbdddca3e09d4497c50745a55e0a3
Lucy Bain <lucybain@gmail.com>
2014-04-14 19:01:55 +1000

    Solution for 1.09

--- Lesson_1_Problem_Set/09-Rounding_Numbers/solutions.py
+++ Lesson_1_Problem_Set/09-Rounding_Numbers/solutions.py
@@ -5,3 +5,18 @@
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+# Add .5
+# ex: 1 -> 1.5
+# ex: 103.7 -> 104.2
+x = x + .5
+# Make x a string (so we can do string manipulation on it)
+x = str(x)
+# Find the position of the demimal point for your number
+# ex: 1.5 -> decimal point is at position 1
+# ex: 103.7 -> decimal point is at posiiton 3
+point = x.find('.')
+# only take the part of the string before the decimal point
+# ex: 1
+# ex: 104
+print x[:point]
