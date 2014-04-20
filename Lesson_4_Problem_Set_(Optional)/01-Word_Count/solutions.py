adbe48006611d77820e00826340991a4e9df4800
tracymu <tracy.musung@moomumedia.com>
2014-04-20 18:39:47 +1000

    Solution to Lesson 3 Problem Set Optional - Word Count

--- Lesson_4_Problem_Set_(Optional)/01-Word_Count/solutions.py
+++ Lesson_4_Problem_Set_(Optional)/01-Word_Count/solutions.py
@@ -1,5 +1,14 @@
 # Please write SOLUTIONS here
 
+def count_words(str):
+    if str=="" or str==" ":
+        return 0
+    else:
+        count=1
+        for i in str:
+            if i ==" ":
+               count+=1
+        return count
 # From the README:
 # We do NOT accept pull requests that have deleted another contributer's hint or solution without a very clear reason
 # ALL solutions must be clearly documented
