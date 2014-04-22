43e8a75e76f5c3e4caba94edbc9ed20418d977ce
tracymu <tracy.musung@moomumedia.com>
2014-04-22 16:38:28 +1000

    Added in comments to Chap 4, Optional Qs, Word Count

--- Lesson_4_Problem_Set_(Optional)/01-Word_Count/solutions.py
+++ Lesson_4_Problem_Set_(Optional)/01-Word_Count/solutions.py
@@ -1,7 +1,19 @@
 # Please write SOLUTIONS here
-
 # From the README:
 # We do NOT accept pull requests that have deleted another contributer's hint or solution without a very clear reason
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+def count_words(str):
+
+    if str=="" or str==" ": # Account for the case where there are no words in the string
+        return 0
+    else:
+        count=1 #If the string isn't empty, or just a space, then there will be at least one word, so start count at 1
+        for i in str:
+        	# Go through the string, and every time you find a space, add to count.
+            if i ==" ":
+               count+=1
+        return count
+
