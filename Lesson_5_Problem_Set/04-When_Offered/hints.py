2e5c0a45289f1ae9a8f5b43523104f006eb24e83
tracymu <tracy.musung@moomumedia.com>
2014-04-29 15:39:00 +1000

    Lesson 5 Problems, 04 when offered, hints and solution

--- Lesson_5_Problem_Set/04-When_Offered/hints.py
+++ Lesson_5_Problem_Set/04-When_Offered/hints.py
@@ -4,3 +4,30 @@
 # We do NOT accept pull requests that have deleted another contributer's hint or solution without a very clear reason
 # ONLY use concepts covered in the class so far
 # Do NOT give solutions here - ONLY hints
+
+
+
+#When looking at problems using dictionaries, you might find it useful to print out the different elements to understand where you are in the code.
+
+#For example:
+
+#Print out courses:
+def when_offered(courses,course):
+    return courses
+
+print when_offered (courses, 'cs101')
+
+#Print out elements inside courses:
+def when_offered(courses,course):
+    for hexamester in courses:
+        print hexamester
+
+when_offered (courses, 'cs101')
+
+#Print out hashes inside courses:
+def when_offered(courses,course):
+    for hexamester in courses:
+        print courses[hexamester]
+
+when_offered (courses, 'cs101')
+
--- Lesson_5_Problem_Set/04-When_Offered/solutions.py
+++ Lesson_5_Problem_Set/04-When_Offered/solutions.py
@@ -5,3 +5,14 @@
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+
+def when_offered(courses,course):
+    result = [] # The question asks you to return a list, so you should start with an empty one.
+    for hexamester in courses: # Iterate through the first level within the courses
+        if course in courses[hexamester]: # Check if course equals the course you are up to in your iteration.
+        																	# Note that you need to say courses[hexamester] not just hexamester.
+        																	# Hexamester will refer only to the top level item you are iterating on , it is just a name(e.g. apr2012)
+        																	# courses[hexamester] is a key, and will return the relevant value, which is the rest of the dictionary.
+            result.append(hexamester) 		# Since you want a list of hexamesters as strings for the result, you can simply append hexamester
+    return result
