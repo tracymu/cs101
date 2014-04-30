716dd900593322d1216b471bfab6b0ab56d48e70
Nadia Vu <winter.nga@gmail.com>
2014-04-30 12:35:15 +0000

    Addedthe Read Me

--- Lesson_2_Problem_Set_(Optional)/01-Weekend/solutions.py
+++ Lesson_2_Problem_Set_(Optional)/01-Weekend/solutions.py
@@ -1,3 +1,9 @@
+# From the README:
+# We do NOT accept pull requests that have deleted another contributer's hint or solution without a very clear reason
+# ALL solutions must be clearly documented
+# ALL solutions must actually work
+# ONLY use concepts covered in the class so far
+
 # There are two solutions below. 
 # The second one is maybe more suited to the material given so far while the first one has less lines
 def weekend(day):
