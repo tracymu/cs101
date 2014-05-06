61bf7e9ab472ffa88ec0cb784858f18aee8e3357
tracymu <tracy.musung@moomumedia.com>
2014-05-06 21:06:53 +1000

    Lesson 5 Memoization

--- Lesson_5_Problem_Set/07-Memoization/solutions.py
+++ Lesson_5_Problem_Set/07-Memoization/solutions.py
@@ -5,3 +5,9 @@
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+def cached_execution(cache, proc, proc_input):
+  if not proc_input in cache:    # Check for the proc_input in the cache (dictionary)
+      cache[proc_input]= proc(proc_input) #If it isn't in there, add a new entry to the dictionary 'cache'
+  return cache[proc_input] # Whether it was already in there, or just added, you can now return the value for that key 'proc_input'
+
